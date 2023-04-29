a56d7c31ec4445c42ac7060444d8344cce74179a
Erick Marques <erick.marquesc@hotmail.com>
2023-04-29 20:33:16 -0300

    refactor: :zap: add component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,22 @@ import { Container, Content } from '@/components/Home/container'
 import { Footer } from '@/components/Home/Footer'
 import { Header } from '@/components/Home/Header'
 import { Hero } from '@/components/Home/Hero'
+import { Main } from '@/components/Home/Main'
 
 import { Nunito } from 'next/font/google'
 const nunito = Nunito({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <Container className={`${nunito.className}`}>
-      <Content>
-        <Header />
-        <Hero/>
-        <Footer />
-      </Content>
-    </Container>
+    <>
+      <Container className={`${nunito.className}`}>
+        <Content>
+          <Header />
+          <Hero />
+          <Footer />
+        </Content>
+      </Container>
+      <Main />
+    </>
   )
 }
