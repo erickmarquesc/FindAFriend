888f9a840883441b437b0714a6b0284de417478a
Erick Marques <erick.marquesc@hotmail.com>
2023-10-10 18:00:22 -0300

    feat: :construction: work in progress

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { HeroSection } from "@/Components/HeroSection"
+import { Container, Content } from "../../Components/Containers/styles"
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection/>
+    </>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>
-        Home page 1
-      </h1>
-    </main>
-  )
-}
